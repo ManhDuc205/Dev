0b6e14a85a8ee4f02270e98f26eb24d564f04560
Nguyễn Mạnh Đức <nguyenduc2005qo@gmail.com>
2025-03-23 22:56:35 +0700

    23/03/2025

--- Data Structure and Algorithms/Cây Nhị Phân/Cay_bieu_thuc_1.cpp
+++ Data Structure and Algorithms/Cây Nhị Phân/Cay_bieu_thuc_1.cpp
@@ -1,22 +1,28 @@
-#include <bits/stdc++.h>
+#include <stack>
+#include <iostream>
+#include <string>
 using namespace std;
 
 // Định nghĩa cấu trúc node của cây biểu thức
 struct Node {
-    string value;
+    string data;
     Node* left;
     Node* right;
     
-    Node(string val) {
-        value = val;
-        left = right = nullptr;
-    }
 };
+
+Node *makeNode(string x){
+    Node *newNode = new Node;
+    newNode->data = x;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
 
 // Hàm kiểm tra xem một ký tự có phải là toán tử không
 bool isOperator(char c) {
     return (c == '+' || c == '-' || c == '*' || c == '/');
 }
+
 
 // Hàm xây dựng cây biểu thức từ biểu thức hậu tố
 Node* constructTree(string postfix) {
@@ -25,14 +31,14 @@ Node* constructTree(string postfix) {
     for (char c : postfix) {
         if (!isOperator(c)) {
             // Nếu là toán hạng, tạo node mới và đưa vào stack
-            st.push(new Node(string(1, c)));
+            st.push(makeNode(string(1, c)));
         } else {
             // Nếu là toán tử, lấy hai node con từ stack
             Node* right = st.top(); st.pop();
             Node* left = st.top(); st.pop();
             
             // Tạo node toán tử mới và gán hai node con
-            Node* opNode = new Node(string(1, c));
+            Node* opNode = makeNode(string(1, c));
             opNode->left = left;
             opNode->right = right;
             
@@ -46,28 +52,28 @@ Node* constructTree(string postfix) {
 }
 
 // Hàm duyệt cây theo thứ tự giữa (infix) với dấu ngoặc tối ưu
-string inorderTraversal(Node* root) {
+string inorder(Node* root) {
     if (!root) return "";
     
     // Nếu là toán hạng, chỉ cần trả về giá trị
-    if (!isOperator(root->value[0])) 
-        return root->value;
+    if (!isOperator(root->data[0])) 
+        return root->data;
     
     // Nếu là toán tử, thêm dấu ngoặc để giữ thứ tự
-    return inorderTraversal(root->left) + " " + root->value + " " + inorderTraversal(root->right);
+    return inorder(root->left) + " " + root->data + " " + inorder(root->right);
 }
 
 int main() {
-    int T;
-    cin >> T;
+    int t;
+    cin >> t;
     cin.ignore();
     
-    while (T--) {
+    while (t--) {
         string postfix;
         cin >> postfix;
         
         Node* root = constructTree(postfix);
-        cout << inorderTraversal(root) << endl;
+        cout << inorder(root) << endl;
     }
     
     return 0;
