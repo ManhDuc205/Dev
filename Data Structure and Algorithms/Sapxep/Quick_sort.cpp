5b0ba11555049cda2e698114e2890b3a09d6ff17
Nguyễn Mạnh Đức <nguyenduc2005qo@gmail.com>
2025-03-27 21:50:30 +0700

    27/03/2025

--- Data Structure and Algorithms/Sapxep/Quick_sort.cpp
+++ Data Structure and Algorithms/Sapxep/Quick_sort.cpp
@@ -3,43 +3,24 @@
 
 using namespace std;
 
-int partition(int a[], int l, int r){ // phân hoạch lomuto
-    int pilot = a[r];
-    int i = l - 1;
-    for(int j = l; j < r; j++){
-        if(a[j] <= pilot){
-            ++i;
-            swap(a[i], a[j]);   
+void QuickSort(int a[], int left, int right){
+    int x = a[(left + right) / 2];
+    int i = left, j = right;
+    do{
+        while(a[i] < x) i++;
+        while(a[j] > x) j--;
+        if(i <= j){
+            swap(a[i], a[j]);
+            i++; j--;
         }
+    }while (i <= j);
+    if(left < j){
+        QuickSort(a, left, j);
+    }
+    if(i < right){
+       QuickSort(a, i, right);
     }
     
-    // Đưa con trỏ về giữa
-    ++i;
-    swap(a[i], a[r]);
-    return i;
-}
-
-int partition2(int a[], int l, int r){ // phân hoạch hoare
-    int pivot = a[l];
-    int i = l - 1, j = r + 1;
-    while(1){
-        do{
-            ++i;
-        }while (a[i] < pivot);
-        do{
-            --j;
-        }while(a[j] > pivot);
-        if( i < j ){
-            swap(a[i], a[j]);
-        }        
-        else return j;
-    }
-}
-void quickSort(int a[], int l, int r){
-    if(l >= r) return;
-    int p = partition(a, l, r);
-    quickSort(a, l, p - 1);
-    quickSort(a, p + 1, r);
 }
 int main(){
     int n; cin >> n;
@@ -47,7 +28,7 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> a[i];
     }
-    quickSort(a, 0, n - 1);
+    QuickSort(a, 0, n - 1);
     for(int i = 0; i < n; i++){
         cout << a[i] << " ";
     }
