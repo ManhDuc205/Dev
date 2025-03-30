66b948e7cbf1f14b0af8b9feb3d95dea1a6b7232
Nguyễn Mạnh Đức <nguyenduc2005qo@gmail.com>
2025-03-30 22:52:59 +0700

    30/03/2025

---
+++ DSA_PTIT_Solution/CTDL_001.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int n, a[100], final = 0;
+void ktao(){
+    for(int i = 1; i <= n; i++){
+        a[i] = 0;
+    }
+}
+
+void sinh(){
+    int i = n;
+    while(i >= 1 && a[i] == 1){
+        a[i] = 0;
+        --i;
+    }
+    if(i == 0){
+        final = 1;
+    }
+    else{
+        a[i] = 1;
+    }
+}
+
+bool laXauNPThuanNghich(){
+    for(int i = 1; i <= n / 2; i++){
+        if(a[i] != a[n - i + 1]) return false;
+    }
+    return true;
+}
+
+int main(){
+    cin >> n;
+    ktao();
+    while(final == 0){
+        if(laXauNPThuanNghich()){
+            for(int i = 1; i <= n; i++){
+                cout << a[i] << " ";
+            }
+            cout << endl;
+        }
+        sinh();
+    }
+}
